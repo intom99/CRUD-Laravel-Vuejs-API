b551989a82818d7745ddd8047e399d6bc0957e3f
intom <iin.arahmah99@gmail.com>
2021-07-08 01:12:40 +0700

    update app-js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,42 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter)
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+import App from './App.vue';
+Vue.use(VueAxios, axios);
+
+import IndexComponent from './components/products/Index.vue';
+import CreateComponent from './components/products/Create.vue';
+import EditComponent from './components/products/Edit.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'index',
+        component: IndexComponent
+    },
+    {
+        path: '/create',
+        name: 'create',
+        component: CreateComponent
+    },
+    {
+        path: '/edit/:id',
+        name: 'edit',
+        component: EditComponent
+    }
+];
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+
+const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');

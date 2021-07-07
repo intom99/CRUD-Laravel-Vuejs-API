545b507b29a3bcb47f5fbe221ecbd2864eb8b351
intom <iin.arahmah99@gmail.com>
2021-07-07 19:07:37 +0700

    controller: func update & destroy

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -50,9 +50,62 @@ class ProductsController extends Controller
     {
         $product = Product::findOrFail($id);
 
-        return response()->json([
-            'message' => 'Detail of product',
-            'data' => $product
-        ], 200);
+        if ($product) {
+            return response()->json([
+                'message' => 'Detail of product',
+                'data' => $product
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => "Data not found",
+                'data' => ''
+            ], 404);
+        }
+    }
+
+    public function update(Request $resquest, $id)
+    {
+        $product = Product::findOrFail($id);
+        // validate data
+        $validator = Validator::make($resquest->all(), [
+            'product_name' => ['required'],
+            'stock' => ['required'],
+            'price' => ['required'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Input data, here!',
+                'data' => $validator->errors()
+            ], 400);
+        } else {
+
+            $product->update($resquest->all());
+            if ($product) {
+                return response()->json([
+                    'message' => 'Data updated'
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Data failed to update'
+                ], 500);
+            }
+        }
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        if ($product) {
+            return response()->json([
+                'message' => 'data deleted'
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'data failed to delete'
+            ], 500);
+        }
     }
 }

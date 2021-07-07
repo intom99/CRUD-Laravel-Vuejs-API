417c51ecc385693a64f7bae403f3b92d8f334c8a
intom <iin.arahmah99@gmail.com>
2021-07-07 18:29:08 +0700

    controller : func store

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductsController extends Controller
 {
@@ -11,8 +12,37 @@ class ProductsController extends Controller
     {
         $product = Product::orderBy('id', 'asc')->get();
         return response()->json([
-            'message' => '',
+            'message' => 'view all product',
             'data' => $product
         ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        // validate data
+        $validator = Validator::make($request->all(), [
+            'product_name' => ['required'],
+            'stock' => ['required'],
+            'price' => ['required'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'input data, here!',
+                'data' => $validator->errors()
+            ], 400);
+        } else {
+            $product = Product::create($request->all());
+
+            if ($product) {
+                return response()->json([
+                    'message' => 'Saved data'
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'data failed to save'
+                ], 400);
+            }
+        }
     }
 }
